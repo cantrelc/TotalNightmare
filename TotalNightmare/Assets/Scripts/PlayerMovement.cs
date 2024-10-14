15d6dd18eb8442db5291ee3fe255361ec1c3b3a3
Nick Abraham <nrabraha@mtu.edu>
2024-10-14 13:23:19 -0400

    Modified to grid movement
    Also added tiles for the bedroom

--- TotalNightmare/Assets/Scripts/PlayerMovement.cs
+++ TotalNightmare/Assets/Scripts/PlayerMovement.cs
@@ -5,20 +5,38 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-    public Rigidbody2D rb;
-    Vector2 movement;
+    public Transform movePoint;
+
+    public LayerMask whatStopsMovement;
+
+    private void Start()
+    {
+        movePoint.parent = null;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        //handle input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-    }
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, speed * Time.deltaTime);
 
-    private void FixedUpdate()
-    {
-        //handle movement
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        {
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + 
+                    new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, whatStopsMovement))
+                {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+                
+            } else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position +
+                    new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, whatStopsMovement))
+                {
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+            }
+        }
     }
 }
